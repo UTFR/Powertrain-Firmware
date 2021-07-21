0bf6416879de9f76a144768864c89b7f15f6132d
Jake Sprenger <jake.sprenger@mail.utoronto.ca>
2021-07-21 00:11:04 -0400

    Useful files for formatting + some macros

---
+++ libs/lib_util.h
@@ -0,0 +1,28 @@
+/* This file contains helpful macros and constants for
+ * the University of Toronto's Formula SAE Racing Team.
+ * 
+ * Contributors: Jake Sprenger, 2021
+ */
+
+#ifndef LIB_UTIL_H
+#define LIB_UTIL_H
+
+#define BIT_X(pos, size)                    ((uint##size##_t)((uint##size##_t)1U << ((uint_least8_t)(pos))))
+#define GET_BIT_X(data, pos, size)          ((bool)(((uint##size##_t)(data) & BIT_X((pos), size)) != 0U))
+#define SET_BIT_X(data, pos, size)          ((uint##size##_t)(data) | BIT_X((pos), size))
+
+#define GET_BIT_U8(data, pos)               GET_BIT_X((data), (pos), 8)
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(arr)                     ((size_t)(sizeof(arr)/sizeof((arr)[0])))
+#endif
+
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+
+#ifndef UNUSED
+#define UNUSED(x) (void)(x)
+#endif
+
+#endif /* LIB_UTIL_H
+
