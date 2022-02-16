7d4f8c53800baca93e6eadd2309c7ea5152a6e4b
Spencer Ball <74116456+SpencerBall99@users.noreply.github.com>
2022-02-16 17:19:17 -0500

    - _APPS_out_verify is now an int. Check deviation between normalized throttle values, not raw readings. Rounded _APPS_output for more accuracy.

--- APPS/libraries/UTFR_APPS_DAC/UTFR_APPS_DAC.cpp
+++ APPS/libraries/UTFR_APPS_DAC/UTFR_APPS_DAC.cpp
@@ -25,14 +25,14 @@ void UTFR_APPS_DAC::processThrottlePosition()
     _APPS_2_in = analogRead(A1);
     _APPS_out_verify = analogRead(A5);
 
-    // model each APPS as a straight line
-    _APPS_1_throttle = _kANALOG_MAX * (_APPS_1_in / _kANALOG_MAX - _kAPPS_1_LOW) / (_kAPPS_1_HIGH - _kAPPS_1_LOW);
-    _APPS_2_throttle = _kANALOG_MAX * (_APPS_2_in / _kANALOG_MAX - _kAPPS_2_LOW) / (_kAPPS_2_HIGH - _kAPPS_2_LOW);
+    // Normalize pedal position readings and multiply by _kANALOG_MAX so we can compare them
+    _APPS_1_throttle = _kANALOG_MAX * ( (_APPS_1_in / _kANALOG_MAX) - _kAPPS_1_LOW) / (_kAPPS_1_HIGH - _kAPPS_1_LOW);
+    _APPS_2_throttle = _kANALOG_MAX * ( (_APPS_2_in / _kANALOG_MAX) - _kAPPS_2_LOW) / (_kAPPS_2_HIGH - _kAPPS_2_LOW);
 
     // set flags
-    _throttle_good = getDeviation(_APPS_1_in, _APPS_2_in) < _kTHROTTLE_MAX_DEVIATION;
+    _throttle_good = getDeviation(_APPS_1_throttle, _APPS_2_throttle) < _kTHROTTLE_MAX_DEVIATION;
     _output_good = getDeviation(_APPS_output, _APPS_out_verify) < _kOUTPUT_DEVIATION;
-    _error_flag_set = _time_at_error != _kBASE_TIME;
+    _error_flag_set = (_time_at_error != _kBASE_TIME);
 
     //=========OUTPUT PROCESSING==========//
 
@@ -53,7 +53,7 @@ void UTFR_APPS_DAC::processThrottlePosition()
         }
         else if (_error_flag_set)
         {
-            _exceed_time_allowance = (_time_now - _time_at_error) >= _kTIME_ALLOWANCE;
+            _exceed_time_allowance = (_time_now - _time_at_error >= _kTIME_ALLOWANCE);
         }
         else
         {
@@ -69,7 +69,8 @@ int UTFR_APPS_DAC::getDeviation(int value_1, int value_2)
 
 void UTFR_APPS_DAC::sendOutput()
 {
-    _APPS_output = (_APPS_1_throttle + _APPS_2_throttle) / 2; // send out the mean of both inputs
+    _APPS_output = round((_APPS_1_throttle + _APPS_2_throttle) / 2);      // take average and round
+    
     analogWrite(_APPS_output, 0);
 }
 
@@ -84,5 +85,5 @@ void UTFR_APPS_DAC::reportError()
 void UTFR_APPS_DAC::shutDown()
 {
     analogWrite(0, 0);  // send out zero throttle
-    // ADD COMMS WITH MEGA HERE (Tell mega to disconnect from chassis ground)
+    // ADD COMMS WITH MEGA HERE (Tell mega to trip SDC/ disconnect LV from chassis ground)
 }
--- APPS/libraries/UTFR_APPS_DAC/UTFR_APPS_DAC.h
+++ APPS/libraries/UTFR_APPS_DAC/UTFR_APPS_DAC.h
@@ -1,5 +1,6 @@
+#include <Arduino.h>
+
 #include "MCP_DAC.h"
-//#include "UTFR_Error.h"
 
 class UTFR_APPS_DAC : public MCP4911 {                       // APPS_DAC inherits all of MCP4911's methods and properties
 
@@ -8,23 +9,23 @@ class UTFR_APPS_DAC : public MCP4911 {                       // APPS_DAC inherit
         UTFR_APPS_DAC(uint8_t dataOut = 255, uint8_t clock = 255);
         void processThrottlePosition();
 
-    protected:
+    private:
 
         const int _kANALOG_MAX = 1023;
-        const int _kAPPS_1_HIGH = 4.75;
-        const int _kAPPS_2_HIGH = 4.275;
-        const int _kAPPS_1_LOW = 1.25;
-        const int _kAPPS_2_LOW = 1.125;
+        const float _kAPPS_1_HIGH = 4.75;     
+        const float _kAPPS_2_HIGH = 4.275;      
+        const float _kAPPS_1_LOW = 1.25;       
+        const float _kAPPS_2_LOW = 1.125;       
 
         const int _kTIME_ALLOWANCE = 100;                // DO NOT CHANGE: Maximum error time allowed by rules; milliseconds
         const float _kTHROTTLE_MAX_DEVIATION = 0.10;
-        const float _kOUTPUT_DEVIATION = 0.03;           // abitrary value for time_now, will be determined experimentally
+        const float _kOUTPUT_DEVIATION = 0.03;           // do we know what we want this to be?
         const long _kBASE_TIME = -1;
 
         int _APPS_1_in = 0;
         int _APPS_2_in = 0;                              // 0 - 1023
         int _APPS_output = 0;
-        float _APPS_out_verify = 0;                      // 0 - 1023
+        int _APPS_out_verify = 0;                        // 0 - 1023
         float _APPS_1_throttle = 0;
         float _APPS_2_throttle = 0;
         long _time_at_error = _kBASE_TIME;                // unsigned long type according to arduino docs, not sure about this
@@ -35,9 +36,10 @@ class UTFR_APPS_DAC : public MCP4911 {                       // APPS_DAC inherit
         bool _exceed_time_allowance = false;
         bool _error_flag_set = false;
 
-        int getDeviation(int value_1, int value_2);
+        static int getDeviation(int value_1, int value_2);
         void sendOutput();
         void reportError();
         void shutDown();
+        int roundOutput(float value);
 
 };
