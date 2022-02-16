31c6f0856c57f94796b3786aa8aab0a9735daa7b
Spencer Ball <74116456+SpencerBall99@users.noreply.github.com>
2022-02-16 17:25:03 -0500

    Pulled Muhammed's updated APPS_code_v2 into this branch.

--- APPS/APPS_code_v2/APPS_code_v2.ino
+++ APPS/APPS_code_v2/APPS_code_v2.ino
@@ -1,25 +1,48 @@
+/******************************************************************************
+ *                              I N C L U D E S                               *
+ *****************************************************************************/
 #include <SPI.h>
 #include "MCP_DAC.h"
 
+/******************************************************************************
+ *                               D E F I N E S                                *
+ *****************************************************************************/
+#define APPS_1_HIGH_ 4.75
+#define APPS_2_HIGH_ 4.275
+#define APPS_1_LOW_ 1.25
+#define APPS_2_LOW_ 1.125
+
+#define DAC_CS_ = 8
+#define TIME_ALLOWANCE_ 100 // maximum error time allowed in milliseconds. rule T.4.2.5
+#define THROTTLE_MAX_DEVIATION_ 0.10 // rule T.4.2.4
+#define OUTPUT_DEVIATION_ 0.03 // abitrary value for time_now, will be determined experimentally
+
+#define BASE_TIME_ -1
+#define ANALOG_CONSTANT 1023
+
+#define GET_DEV(x, y) abs((x-y)/x)
+
+/******************************************************************************
+ *                              T Y P E D E F S                               *
+ *****************************************************************************/
+
+
+/******************************************************************************
+ *         P R I V A T E   F U N C T I O N   D E C L A R A T I O N S          *
+ *****************************************************************************/
+void sendOutput();
+void reportError();
+void shutDown();
+
+/******************************************************************************
+ *              P R I V A T E   D A T A   D E F I N I T I O N S               *
+ *****************************************************************************/
 MCP4911 DAC; // HW DAC
 
-// declare global constants
-const int APPS_1_HIGH_ = 4.75;
-const int APPS_2_HIGH_ = 4.275;
-const int APPS_1_LOW_ = 1.25;
-const int APPS_2_LOW_ = 1.125;
-
-const int DAC_CS_ = 8;
-const int TIME_ALLOWANCE_ = 100; // maximum error time allowed in milliseconds
-const float THROTTLE_MAX_DEVIATION_ = 0.10;
-const float OUTPUT_DEVIATION_ = 0.03; // abitrary value for time_now, will be determined experimentally
-const long BASE_TIME_ = -1;
-
-// declare variables
 int APPS_1_in = 0;
-int APPS_2_in = 0; //0 - 1023
+int APPS_2_in = 0; 
 int APPS_output = 0;
-float APPS_out_verify = 0; //0 - 1023
+float APPS_out_verify = 0; 
 float APPS_1_throttle = 0;
 float APPS_2_throttle = 0;
 long time_at_error = BASE_TIME_; // unsigned long type according to arduino docs, not sure about this
@@ -30,11 +53,44 @@ bool output_good = false;
 bool exceed_time_allowance = false;
 bool error_flag_set = false;
 
-//========FUNCTION DECLARATIONS========//
-//int getDeviation(int value_1, int value_2);
-//void sendOutput();
-//void reportError();
-//void shutDown();
+/******************************************************************************
+ *                     P R I V A T E   F U N C T I O N S                      *
+ *****************************************************************************/
+void sendOutput()
+{
+  APPS_output = (APPS_1_throttle + APPS_2_throttle) / 2; // send out the mean of both inputs
+  DAC.analogWrite(APPS_output, 0);
+}
+
+// Reports when implausibility occurs for more than 100ms 
+// Basic implementation for now
+void reportError()
+{
+  if (!throttle_good)
+  {
+    Serial.println("[APPS] Error: throttle values not in expected range of each other.");
+  }
+  else if (!output_good)
+  {
+    Serial.println("[APPS] Error: throttle value was output correctly.");
+  }
+}
+
+// Some logic to shutdown the car
+// Might have to send other info over SPI
+void shutDown()
+{
+  // More post processing
+}
+
+/******************************************************************************
+ *                      P U B L I C   F U N C T I O N S                       *
+ *****************************************************************************/
+
+
+/******************************************************************************
+ *                    P R O G R A M   E X E C U T I O N                       *
+ *****************************************************************************/
 
 void setup()
 {
@@ -59,15 +115,15 @@ void loop()
   APPS_out_verify = analogRead(A5);
 
   // model each APPS as a straight line
-  APPS_1_throttle = 1023 * (APPS_1_in / 1023 - APPS_1_LOW_) / (APPS_1_HIGH_ - APPS_1_LOW_);
-  APPS_2_throttle = 1023 * (APPS_2_in / 1023 - APPS_2_LOW_) / (APPS_2_HIGH_ - APPS_2_LOW_);
+  APPS_1_throttle = ANALOG_CONSTANT * (APPS_1_in / ANALOG_CONSTANT - APPS_1_LOW_) / (APPS_1_HIGH_ - APPS_1_LOW_);
+  APPS_2_throttle = ANALOG_CONSTANT * (APPS_2_in / ANALOG_CONSTANT - APPS_2_LOW_) / (APPS_2_HIGH_ - APPS_2_LOW_);
 
   // set flags
-  throttle_good = getDeviation(APPS_1_in, APPS_2_in) < THROTTLE_MAX_DEVIATION_;
-  output_good = getDeviation(APPS_output, APPS_out_verify) < OUTPUT_DEVIATION_;
-  error_flag_set = time_at_error != BASE_TIME_;
+  throttle_good = GET_DEV(APPS_1_throttle, APPS_2_throttle) < THROTTLE_MAX_DEVIATION_;
+  output_good = GET_DEV(APPS_output, APPS_out_verify) < OUTPUT_DEVIATION_;
+  error_flag_set = (time_at_error != BASE_TIME_);
 
-  //=========OUTPUT PROCESSING==========//
+  //========= O U T P U T    P R O C E S S I N G ==========//
 
   if (exceed_time_allowance)
   {
@@ -82,44 +138,17 @@ void loop()
 
     if (throttle_good && output_good)
     {
+      // reset the error flags
       error_flag_set = false;
       time_at_error = BASE_TIME_;
     }
     else if (error_flag_set)
     {
-      exceed_time_allowance = (time_now - time_at_error) >= TIME_ALLOWANCE_;
+      exceed_time_allowance = (time_now - time_at_error >= TIME_ALLOWANCE_);
     }
     else
     {
       time_at_error = time_now;
     }
   }
-}
-
-//==========FUNCTION DEFINITIONS==========//
-
-int getDeviation(int value_1, int value_2)
-{
-  return abs((value_1 - value_2) / value_1);
-}
-
-void sendOutput()
-{
-  APPS_output = (APPS_1_throttle + APPS_2_throttle) / 2; // send out the mean of both inputs
-  DAC.analogWrite(APPS_output, 0);
-}
-
-void reportError()
-{
-  if (!throttle_good)
-    Serial.println("[APPS] Error: throttle values not in expected range of each other.");
-  else if (!output_good)
-    Serial.println("[APPS] Error: throttle value was output incorrectly.");
-}
-
-void shutDown()
-{
-  //=======More post processing=======//
-  // some logic to shutdown the car
-  // might have to send other info over SPI
 }
