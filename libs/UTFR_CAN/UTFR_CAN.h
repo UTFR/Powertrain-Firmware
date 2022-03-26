e7399eb65d39965895cdb1585b3f34a0e6517935
kelvin-cui <kelvin.cui@mail.utoronto.ca>
2022-03-26 18:20:02 -0400

    tested Error logic, added new fields to UTFR_CAN message

--- libs/UTFR_CAN/UTFR_CAN.h
+++ libs/UTFR_CAN/UTFR_CAN.h
@@ -38,14 +38,15 @@
 
 // ER1 - Error CAN Message 1
 #define ER_APPS_MISMATCH    1
-#define ER_SDC_TRIPPED      2
+#define ER_APPS_OUTPUT      2
+#define ER_SDC_TRIPPED      3
 
 enum CAN_msgNames_E                          // Define all CAN message names here so you can access them by name later                                    
-        {                                           
+        {     
+            CAN_MSG_ER0,
+            CAN_MSG_ER1,                                      
             CAN_MSG_RF0,  
             CAN_MSG_RF1,
-            CAN_MSG_ER0,
-            CAN_MSG_ER1,
 
             CAN_MSG_COUNT
         };
@@ -94,6 +95,32 @@ class UTFR_CAN
 
         CAN_msg_S _CAN_msgArray[CAN_MSG_COUNT] =                        // Initialize CAN message array   
         {
+            [CAN_MSG_ER0] = 
+            {
+                .msgID = 0x1B2, //TBD
+                .msgData = {0xFF, 0xFF, 0xFF, 0xFF, 
+                            0xFF, 0xFF, 0xFF, 0xFF},
+                .msgFields = {ER_BMS_OVERTEMP_F,  ER_BMS_OVERVOLT_F, 
+                              ER_BMS_UNDERVOLT_F, UNUSED_F,
+                              UNUSED_F,     UNUSED_F,
+                              UNUSED_F,     UNUSED_F},
+                .isTx = true,
+                .isRx = false,
+                .isDirty = false,
+            },
+            [CAN_MSG_ER1] = 
+            {
+                .msgID = 0x1B3, //TBD
+                .msgData = {0xFF, 0xFF, 0xFF, 0xFF, 
+                            0xFF, 0xFF, 0xFF, 0xFF},
+                .msgFields = {ER_APPS_MISMATCH,  ER_APPS_OUTPUT, 
+                              ER_SDC_TRIPPED,     UNUSED_F,
+                              UNUSED_F,     UNUSED_F,
+                              UNUSED_F,     UNUSED_F},
+                .isTx = true,
+                .isRx = false,
+                .isDirty = false,
+            },
             [CAN_MSG_RF0] = 
             {
                 .msgID = 0x1B0,
@@ -116,32 +143,6 @@ class UTFR_CAN
                               FW_STRAIN_TIP_F, FW_STRAIN_TIP_F,
                               STEER_ANG_F,     STEER_ANG_F,
                               UNUSED_F,        UNUSED_F},
-                .isTx = true,
-                .isRx = false,
-                .isDirty = false,
-            },
-            [CAN_MSG_ER0] = 
-            {
-                .msgID = 0x1B2, //TBD
-                .msgData = {0xFF, 0xFF, 0xFF, 0xFF, 
-                            0xFF, 0xFF, 0xFF, 0xFF},
-                .msgFields = {ER_BMS_OVERTEMP_F,  ER_BMS_OVERVOLT_F, 
-                              ER_BMS_UNDERVOLT_F, UNUSED_F,
-                              UNUSED_F,     UNUSED_F,
-                              UNUSED_F,     UNUSED_F},
-                .isTx = true,
-                .isRx = false,
-                .isDirty = false,
-            },
-            [CAN_MSG_ER1] = 
-            {
-                .msgID = 0x1B3, //TBD
-                .msgData = {0xFF, 0xFF, 0xFF, 0xFF, 
-                            0xFF, 0xFF, 0xFF, 0xFF},
-                .msgFields = {ER_APPS_MISMATCH,  ER_SDC_TRIPPED, 
-                              UNUSED_F,     UNUSED_F,
-                              UNUSED_F,     UNUSED_F,
-                              UNUSED_F,     UNUSED_F},
                 .isTx = true,
                 .isRx = false,
                 .isDirty = false,
