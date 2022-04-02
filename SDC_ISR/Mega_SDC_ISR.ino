ae1c37e46713485b41dc11afee962af95f4595c9
Tunc <tuncgonelfb@hotmail.com>
2022-04-01 23:58:07 -0400

    Shutdown changes

--- SDC_ISR/Mega_SDC_ISR.ino
+++ SDC_ISR/Mega_SDC_ISR.ino
@@ -2,7 +2,6 @@
  *                              I N C L U D E S                               *
  *****************************************************************************/
 
-#include <string>
 #include <SoftwareSerial.h>
 
 /******************************************************************************
@@ -11,6 +10,7 @@
 
 #define HW_PIN_MSCOM1 40
 #define HW_PIN_MSCOM2 41
+#define HW_PIN_MSCOM3 20
 
 /******************************************************************************
  *                              T Y P E D E F S                               *
@@ -30,9 +30,8 @@
 const int kRETRY_INTERVAL = 20;
 unsigned long current_time, start_time = 0;
 
-string serialData = " ";
 bool SDC_TRIPPED = false;
-
+int checkState = 0;                                 // Checks the state for the  20 pin to ensure that zero torque has been sent to the inverter
 
 /******************************************************************************
  *                     P R I V A T E   F U N C T I O N S                      *
@@ -43,8 +42,20 @@ void SDC_ISR(){
   return SDC_TRIPPED;
 }
 
-void shutdown(){
-  while(1){}
+
+
+void shutdown_carOff()                             
+{
+  digitalWrite(38, LOW);                            // Tell inverter to shutdown
+  digitalWrite(7, LOW);                             // LV Battery Mosfet -> turn off most LV systems
+  while(1)
+  {
+    //ERROR.sendErr(carOff);                        // Kelvin's error library
+    #ifdef debug_RC_Mega
+    Serial.print("Car off. \n";)
+    #endif
+    delay(1000);
+  }
 }
 
 /******************************************************************************
@@ -60,6 +71,7 @@ void setup()
   pinMode(HW_PIN_MSCOM1, OUTPUT);
 
   pinMode(HW_PIN_MSCOM2, INPUT);
+  pinMode(HW_PIN_MSCOM3, INPUT);
   attachInterrupt(digitalPinToInterrupt(19), SDC_ISR, RISING);
 
 }
@@ -69,33 +81,28 @@ void loop()
 
 if (SDC_TRIPPED == true) {
 
-  start_time = millis()
+    start_time = millis()
+    shutdown_carOff();
 
-      current_time = start_time;
-
-  while ((current_time - start_time) < kRETRY_INTERVAL)
-  {
-    current_time = millis()
-
-    if (Serial.available())
-    {
-      serialData = Serial.readStringUntil("/n");
+    if (digitalRead(HW_PIN_MSCOM3) == low) {                  // Checks to make sure that zero torque has been sent to the inverter 
+                                                              
+      shutdown_carOff();                                      // If not send signal back to micro to shutdown             
     }
-    if (serialData != " ")
-    {
-      if (serialData == "SDC1") 
-      {
-        shutdown();// need to make shutdown function
-      }
-      else
-      {
-        Serial.print(serialData);
-      }
-    }
-  }
+    
+    
+    current_time = start_time;
 
-  serialData = " ";
-  digitalWrite(HW_PIN_MSCOM1, LOW);
+
+    if ((current_time - start_time) < kRETRY_INTERVAL)
+    {
+      current_time = millis();
+      digitalWrite(HW_PIN_MSCOM1, LOW);
+      digitalWrite(HW_PIN_MSCOM1, HIGH);
+    
+    }
+    
+    start_time = millis();
+    digitalWrite(HW_PIN_MSCOM1, LOW);
 }
   
   
