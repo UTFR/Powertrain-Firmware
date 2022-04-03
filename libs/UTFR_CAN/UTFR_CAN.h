e3f86836ba5e81667d70c4b2b187ce2f39373080
Spencer Ball <74116456+SpencerBall99@users.noreply.github.com>
2022-04-03 17:41:21 -0400

    Now works with both 2 nodes and 1 node via a #define switch. Updated examples. Updated ReadMe.

--- libs/UTFR_CAN/UTFR_CAN.h
+++ libs/UTFR_CAN/UTFR_CAN.h
@@ -18,7 +18,10 @@
 // ---------->> CHANGE THESE FOR EACH SPECIFIC IMPLEMENTATION OF THIS LIBRARY
 // ----------------------------------------------------------------------------------------------------->>
 
-#define UNUSED_F 0                           // Define your message data fields here so you can access them by name later
+//#define _1_NODE_                            // Comment out for MCU that uses two nodes
+#define _2_NODES_                         // Comment out for MCU that uses only one node
+
+#define UNUSED_F 0                          // Define your message data fields here so you can access them by name later
 
 // RF0 - Right Front corner module CAN message 0
 #define RF_OUT_TIRE_TEMP_F  1               // 1st field
@@ -33,12 +36,13 @@
 
 
 enum CAN_msgNames_E                          // Define all CAN message names here so you can access them by name later                                    
-        {                                           
-            CAN_MSG_RF0,  
-            CAN_MSG_RF1,
+{                                           
+    CAN_MSG_RF0,  
+    CAN_MSG_RF1,
 
-            CAN_MSG_COUNT
-        };
+    CAN_MSG_COUNT
+};
+
 
 // ----------------------------------------------------------------------------------------------------->>
 
@@ -50,7 +54,8 @@ class UTFR_CAN
         MCP_CAN _NODE1;                              // 1st CAN NODE object from "mcp_can.h"
         MCP_CAN _NODE2;                              // 2nd CAN NODE object from "mcp_can.h"
 
-        uint8_t _CS;                                // Chip Select Pin (Initialized by constructor)
+        uint8_t _CS1;                               // Chip Select Pin for node 1 (Initialized by constructor)
+        uint8_t _CS2;                               // Chip Select Pin for node 2 (Initialized by constructor)
         uint8_t _busSpeed;                          // bus speed, pre-defined values in "mcp_can_dfs.h"
         bool _ext = false;                          // Extended format? 11-bit ID for normal format, 29-bit ID for extended
 
@@ -148,21 +153,32 @@ class UTFR_CAN
     *          P U B L I C   F U N C T I O N   D E C L A R A T I O N S            *
     ******************************************************************************/
     public:
-        UTFR_CAN(uint8_t CS);                                                       // Constructor
-        void begin(uint8_t busSpeed, int nodeNumber);                               // Initialize CAN node
-                                                                                    // added nodeNumber parameter to check which node is being used
-                                                                                    // 1 for node 1, 2 for node 2, 12 for both nodes 1 and 2
+        UTFR_CAN(uint8_t CS1);                                                      // Constructor for one node
+        UTFR_CAN(uint8_t CS1, uint8_t CS2);                                         // Constructor for two nodes
 
-        void sendAll(int nodeNumber);                                               // Send all messages in _CAN_msgArray where .isTx == true
-        void sendMsg(CAN_msgNames_E msgName, int nodeNumber);                       // Send message by name
-        void receiveMsgs(int nodeNumber);                                           // get all incoming messages from Rx buffers and update _CAN_msgArray
+        #ifdef _1_NODE_
+        void begin(uint8_t busSpeed);                                               // Initialize CAN node
+        void sendAll(void);                                                         // Send all messages in _CAN_msgArray where .isTx == true
+        void sendMsg(CAN_msgNames_E msgName);                                       // Send message by name
+        void receiveMsgs(void);                                                     // get all incoming messages from Rx buffers and update _CAN_msgArray
+        void setFilters(void);                                              // set masks and filters according to CAN_filterArray
+        void setFilters_permitAll(void);                                    // set masks and filters to receive ALL messages sent by other nodes
+        void setFilters_permitNone(void);                                   // set masks and filters to receive NO messages sent by other nodes
+        #endif
+
+        #ifdef _2_NODES_
+        void begin(uint8_t busSpeed, uint8_t nodeNumber);                           // Initialize CAN node
+                                                                                    // nodeNumber: 1 for node 1, 2 for node 2, 3 for both nodes
+        void sendAll(uint8_t nodeNumber);                                           // Send all messages in _CAN_msgArray where .isTx == true
+        void sendMsg(CAN_msgNames_E msgName, uint8_t nodeNumber);                   // Send message by name
+        void receiveMsgs(uint8_t nodeNumber);                                       // get all incoming messages from Rx buffers and update _CAN_msgArray
+        void setFilters(uint8_t nodeNumber);                                        // set masks and filters according to CAN_filterArray
+        void setFilters_permitAll(uint8_t nodeNumber);                              // set masks and filters to receive ALL messages sent by other nodes
+        void setFilters_permitNone(uint8_t nodeNumber);                             // set masks and filters to receive NO messages sent by other nodes
+        #endif
+
         unsigned long getField(CAN_msgNames_E msgName, uint8_t fieldName);          // Get data from field by name (defined at top of this file)
         void setField(CAN_msgNames_E msgName, uint8_t fieldName, long fieldData);   // Set field data by name, pass in data you want to set as well
-
-        void setFilters(int nodeNumber);                                            // set masks and filters according to CAN_filterArray
-        void setFilters_permitAll(int nodeNumber);                                  // set masks and filters to receive ALL messages sent by other nodes
-        void setFilters_permitNone(int nodeNumber);                                 // set masks and filters to receive NO messages sent by other nodes
-
         void printMsgData(CAN_msgNames_E msgName);                                  // Prints all data stored in a given message (Note: will be in decimal format)
 
         // TO DO: Functions that return void should return error_types defined in an enum. Makes debug easier.
@@ -173,7 +189,7 @@ class UTFR_CAN
      *         P R I V A T E   F U N C T I O N   D E C L A R A T I O N S          *
      *****************************************************************************/
     private:
-        void receiveMsgsCommonFcn();                                                // parts of receiveMsgs common to all nodes
+        void receiveMsgsCommonFcn(unsigned long canID, uint8_t dataLength, uint8_t buf[8]); // parts of receiveMsgs common to all nodes
 };
 
 #endif
