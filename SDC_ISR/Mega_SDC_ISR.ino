ad688915d5e5a6710dc5fdc8c72f2abe38897f9d
Tunc <78280107+TuncG@users.noreply.github.com>
2022-04-03 14:00:19 -0400

    Review 3

--- SDC_ISR/Mega_SDC_ISR.ino
+++ SDC_ISR/Mega_SDC_ISR.ino
@@ -16,12 +16,9 @@
  *                              T Y P E D E F S                               *
  *****************************************************************************/
 
-
 /******************************************************************************
  *         P R I V A T E   F U N C T I O N   D E C L A R A T I O N S          *
  *****************************************************************************/
-
-
 
 /******************************************************************************
  *              P R I V A T E   D A T A   D E F I N I T I O N S               *
@@ -31,30 +28,29 @@ const int kRETRY_INTERVAL = 20;
 unsigned long current_time, start_time = 0;
 
 bool SDC_TRIPPED = false;
-int checkState = 0;                                 // Checks the state for the  20 pin to ensure that zero torque has been sent to the inverter
+int checkState = 0; // Checks the state for the  20 pin to ensure that zero torque has been sent to the inverter
 
 /******************************************************************************
  *                     P R I V A T E   F U N C T I O N S                      *
  *****************************************************************************/
-void SDC_ISR(){
+void SDC_ISR()
+{
   digitalWrite(HW_PIN_MSCOM1, HIGH);
   SDC_TRIPPED = true;
   return SDC_TRIPPED;
 }
 
-
-
-void shutdown_carOff()                             
+void shutdown_carOff()
 {
-  digitalWrite(38, LOW);                            // Tell inverter to shutdown
-  digitalWrite(7, LOW);                             // LV Battery Mosfet -> turn off most LV systems
-  while(1)
+  digitalWrite(38, LOW); // Tell inverter to shutdown
+  digitalWrite(7, LOW);  // LV Battery Mosfet -> turn off most LV systems
+  while (1)
   {
-    //ERROR.sendErr(carOff);                        // Kelvin's error library
-    #ifdef debug_RC_Mega
+// ERROR.sendErr(carOff);                        // Kelvin's error library
+#ifdef debug_RC_Mega
     Serial.print("Car off. \n";)
-    #endif
-    delay(1000);
+#endif
+        delay(1000);
   }
 }
 
@@ -73,37 +69,32 @@ void setup()
   pinMode(HW_PIN_MSCOM2, INPUT);
   pinMode(HW_PIN_MSCOM3, INPUT);
   attachInterrupt(digitalPinToInterrupt(19), SDC_ISR, RISING);
-
 }
 
 void loop()
 {
 
-if (SDC_TRIPPED == true) {
+  if (SDC_TRIPPED == true)
+  {
 
     start_time = millis()
-    shutdown_carOff();
 
-    if (digitalRead(HW_PIN_MSCOM3) == low) {                  // Checks to make sure that zero torque has been sent to the inverter 
-                                                              
-      shutdown_carOff();                                      // If not send signal back to micro to shutdown             
+        if (digitalRead(HW_PIN_MSCOM3) == HIGH)
+    { // Checks to make sure that zero torque has been sent to the inverter
+
+      shutdown_carOff(); // If not send signal back to micro to shutdown
     }
-    
-    
-    current_time = start_time;
 
+    current_time = start_time;
 
     if ((current_time - start_time) < kRETRY_INTERVAL)
     {
       current_time = millis();
       digitalWrite(HW_PIN_MSCOM1, LOW);
       digitalWrite(HW_PIN_MSCOM1, HIGH);
-    
     }
-    
+
     start_time = millis();
     digitalWrite(HW_PIN_MSCOM1, LOW);
-}
-  
-  
+  }
 }
--- SDC_ISR/Micro_SDC_ISR.ino
+++ SDC_ISR/Micro_SDC_ISR.ino
@@ -3,75 +3,70 @@
  *****************************************************************************/
 
 #include "MCP_DAC.h"
-
+#define HW_PIN_MSCOM3 6
 
 /******************************************************************************
  *                              T Y P E D E F S                               *
  *****************************************************************************/
-
 
 /******************************************************************************
  *         P R I V A T E   F U N C T I O N   D E C L A R A T I O N S          *
  *****************************************************************************/
 bool SDC_ISR();
 
-
-
 /******************************************************************************
  *              P R I V A T E   D A T A   D E F I N I T I O N S               *
  *****************************************************************************/
 
-
-MCP4911 DAC;                  // Digital-to-Analog converter -> this sends drive signals to the inverter which, in turn, powers the motor.
+MCP4911 DAC; // Digital-to-Analog converter -> this sends drive signals to the inverter which, in turn, powers the motor.
 
 const int kCS_DAC = 8;
-const int kSDC_INT = 2;
+const int HW_PIN_MSCOM1 = 2;
 
-bool SDC_TRIPPED = false;     // if this is true, do nothing until 0 torque to inverter
+bool SDC_TRIPPED = false; // if this is true, do nothing until 0 torque to inverter
 
 bool written = false;
 int readback = -1;
-
+int kREADBACK_DELAY = 10;
 
 /******************************************************************************
  *                     P R I V A T E   F U N C T I O N S                      *
  *****************************************************************************/
 
-bool SDC_ISR(){
-  DAC.analogWrite(0, 0);            // Write 0 throttle to DAC
-  readback = analogRead(A5); 
+bool SDC_ISR()
+{
+  DAC.analogWrite(0, 0); // Write 0 throttle to DAC
+  delayMicroseconds(kREADBACK_DELAY);
   SDC_TRIPPED = true;
+  readback = analogRead(A5);
 }
 
 /******************************************************************************
  *                      P U B L I C   F U N C T I O N S                       *
  *****************************************************************************/
 
-void setup() {
+void setup()
+{
 
   Serial.begin(115200);
   apps.begin(kCS_DAC);
-  
-  attachInterrupt(digitalPinToInterrupt(kSDC_INT), SDC_ISR, mode);
 
+  attachInterrupt(digitalPinToInterrupt(HW_PIN_MSCOM1), SDC_ISR, RISING);
+  pinmode(HW_PIN_MSCOM3, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
 
-  
-  
-  if (SDC_TRIPPED) {
-    
-    if (readback != 0){
-       SDC_ISR()
+  if (SDC_TRIPPED)
+  {
+    if (readback != 0)
+    {
+      SDC_ISR()
     }
-
-    
-    // TO DO: while readback != 0, try to send 0 torque to inverter
-    // TO DO: if timeout, send failure message to Mega - Mega alerts other systems
-    // TO DO: if success, send success message to Mega
-    // call shutdown function
-    
+    else
+    {
+      digitalwrite(HW_PIN_MSCOM3, LOW);
+    }
   }
-
 }
